b35d775daa099b0960a25e1eb9e7135e37b7cfe7
ljc-dev <62596124+ljc-dev@users.noreply.github.com>
2020-04-01 18:42:04 +0300

    updated description mainly

--- sortAlgorithms.js
+++ sortAlgorithms.js
@@ -3,29 +3,34 @@
 //https://medium.com/javascript-in-plain-english/javascript-merge-sort-3205891ac060
 //https://khan4019.github.io/front-end-Interview-Questions/sort.html#insertionSort
 //https://humanwhocodes.com/blog/2012/11/27/computer-science-in-javascript-quicksort/
-//https://hackernoon.com/elementary-sorting-algorithm-in-javascript-izd1y30x2  <= has a few code mistakes
+//https://medium.com/javascript-algorithms/javascript-algorithms-bubble-sort-3d27f285c3b2
 
 const testArr = [1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]
 
 //BUBBLE SORT +++++++++++++++++++++++++++++++++++++++
+//compare the 1st element with the 2nd, swap the bigger one to 2nd place
+//compare the 2nd element with the 3rd, swap the bigger one to 3rd place
+//etc.
+//when you have reached the end of the array, repeat all over
+//by default nb of repetition = array length
+//to increase efficiency, use a hasSwapped boolean to break the loop and return if nothing has been swapped
 
 function bubbleSort(arr) {
     const n = arr.length;
-    // Iteration of array till last before element 
-    for (let i = 0; i < (n - 1); i++) {
-        //optional swapped tracker => more efficient
+    for (let i = 0; i < n - 1; i++) {
+        //optional swapped tracker => less iterations => more efficient
         let hasSwapped = false;
-        // Iteration of array till last before element 
-        for (j = 0; j < (n - 1); j++) {
+        //full array swap iteration
+        for (let j = 0; j < n - 1; j++) {
             if (arr[j] > arr[j + 1]) {
-                // Swap elements
                 let temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
                 hasSwapped = true;
             }
         }
-        if(!hasSwapped) break;
+        console.log(arr);
+        if (!hasSwapped) break;
     }
     return arr;
 }
@@ -34,49 +39,49 @@ function bubbleSort(arr) {
 
 //INSERTION SORT +++++++++++++++++++++++++++++++++++++++
 
-function insertionSort(arr) {
-    const n = arr.length;
-    // Iteration of array till last element 
-    for (i = 1; i < n; i++) {
-        let j = i;
-        // Iterate over the sorted part of array and insert the element
-        while (j > 0 && arr[j] < arr[j - 1]) {
-            // Swap elements
-            let temp = arr[j];
-            arr[j] = arr[j - 1]
-            arr[j - 1] = temp;
+function insertionSort(array) {
+    for (let i = 0; i < array.length; i++) {
+        let j = i + 1;
+        while (j > 0 && array[j] < array[j - 1]) {
+            let tmp = array[j];
+            array[j] = array[j - 1];
+            array[j - 1] = tmp;
             j--;
         }
     }
-    return arr;
+    console.log(array)
+    return array;
 }
-
 //console.log(insertionSort(testArr));
 
 //SELECTION SORT +++++++++++++++++++++++++++++++++++++++
+//easiest one to understand (for me at least)
+//find the minValue and swap it with the 1st element
+//find the minValue and swap it with the 2nd element 
+//etc.
+//when you have reached the end of the array, return 
 
-function selectionSort(arr) {
-    const n = arr.length;
-    // Iteration of array till last element 
-    for (let i = 0; i < n; i++) {
-        let leastElementIndex = i;
-        for (let j = i + 1; j < arr.length; j++) {
-            // Check for least element and override least element index
-            if (arr[j] < arr[leastElementIndex]) {
-                leastElementIndex = j;
-            }
+function selectionSort(array) {
+    for (let i = 0; i < array.length; i++) {
+        let minIndex = i;
+        for (let j = i; j < array.length; j++) {
+            if (array[j] < array[minIndex]) minIndex = j;
         }
-        // Swap elements
-        let temp = arr[i];
-        arr[i] = arr[leastElementIndex];
-        arr[leastElementIndex] = temp;
+        let tmp = array[i];
+        array[i] = array[minIndex];
+        array[minIndex] = tmp;
     }
-    return arr;
+    console.log(array);
+    return array;
 }
 
 //console.log(selectionSort(testArr));
 
 //MERGE SORT +++++++++++++++++++++++++++++++++++++++
+//divide your array with recursion until all you have are arrays of one item
+//compare each item of each pair of array and merge your way up
+//better explanation at https://khan4019.github.io/front-end-Interview-Questions/sort.html#mergeSort
+
 
 function mergeSort(arr) {
     let len = arr.length;
@@ -84,14 +89,12 @@ function mergeSort(arr) {
     let mid = Math.floor(len / 2),
         left = arr.slice(0, mid),
         right = arr.slice(mid);
-    //send left and right to the mergeSort (=> recursion) to divide again
-    //then merge the divided part
     return merge(mergeSort(left), mergeSort(right));
 }
 
 function merge(left, right) {
-    let result = [], l = 0, r = 0, lLen = left.length, rLen = right.length;
-    while (l < lLen && r < rLen) {
+    let result = [], l = 0, r = 0;
+    while (l < left.length && r < right.length) {
         if (left[l] < right[r]) {
             result[result.length] = left[l];
             l++;
@@ -100,18 +103,18 @@ function merge(left, right) {
             r++;
         }
     }
-    //remaining part needs to be added to the result
+    //add remaining parts
     return result.concat(left.slice(l)).concat(right.slice(r));
 }
 
 //console.log(mergeSort(testArr));
 
 //QUICK SORT +++++++++++++++++++++++++++++++++++++++
+//https://khan4019.github.io/front-end-Interview-Questions/sort.html#quickSort
 
 function partition(arr, left, right) {
-    var pivotValue = arr[Math.floor((right + left) / 2)],
-        left = left,
-        right = right;
+    //get the value of the pivot not the index because
+    let pivotValue = arr[Math.floor((left + right) / 2)];
 
     while (left <= right) {
         while (arr[left] < pivotValue) left++
@@ -123,27 +126,25 @@ function partition(arr, left, right) {
         }
     }
     // value of updated left is called partitionIndex in quickSort
-    //It's called partitionIndex because it's used to delimit two (smaller) partitions to quickSort from: [left, partitionIndex] [partitionIndex, right]
+    //because it's used to delimit two (smaller) partitions: 
+    //[left, partitionIndex - 1] [partitionIndex, right]
     return left;
 }
 
-function swap(arr, firstIndex, secondIndex) {
-    var tmp = arr[firstIndex];
-    arr[firstIndex] = arr[secondIndex];
-    arr[secondIndex] = tmp;
+function swap(arr, index1, index2) {
+    let tmp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = tmp;
 }
 
 function quickSort(arr, left, right) {
-    var partitionIndex;
-    //when divided until arr < 2 => return arr 
+    let partitionIndex;
     if (arr.length > 1) {
         partitionIndex = partition(arr, left, right);
         if (left < partitionIndex - 1) {
-            //recursion
             quickSort(arr, left, partitionIndex - 1);
         }
         if (partitionIndex < right) {
-            //recursion
             quickSort(arr, partitionIndex, right);
         }
     }
